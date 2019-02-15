4c4395b9474561b897d8c7990b9c2164ef566b3c
takanori-hayashi <tk_hayashi@e-create.jp>
2019-02-15 15:08:21 +0900

    all file commit

---
+++ src/bar.js
@@ -0,0 +1,3 @@
+const bar = () => console.log('bar');
+
+export default bar;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },  
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  },
+};
